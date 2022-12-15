29eed646237515871f58b7dd9d8a32afdd7b84e8
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-15 23:16:14 +0600

    HeroImg Html done

--- src/components/Hero/HeroImg.js
+++ src/components/Hero/HeroImg.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./HeroImg.css";
 import IntroImg from "../../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
@@ -8,6 +9,19 @@ const HeroImg = () => {
       <div className="hero">
         <div className="mask">
           <img className="intro-img" src={IntroImg} alt="" />
+        </div>
+        <div className="content">
+          <p>Hello.. I am</p>
+          <h1>Md. Tahmidur Rahman</h1>
+          <p>Web Developer</p>
+        </div>
+        <div>
+          <Link to="/projects" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
         </div>
       </div>
     </div>
