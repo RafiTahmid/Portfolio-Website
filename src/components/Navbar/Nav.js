943189135ff404eb7e0a7365f28b178ddda487e5
RafiTahmid <rafi.tahmid019@gmail.com>
2022-12-15 13:33:18 +0600

    nav added

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <Link to="/">
+        <h2>Portfolio</h2>
+      </Link>
+
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/project">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Nav;
-
-// 24
